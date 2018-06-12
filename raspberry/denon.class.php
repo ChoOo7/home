22192322c1b839a0d591ee2b53e87e29c1589dbf
Simon Minotto <simon.minotto@gmail.com>
2018-06-12 22:03:16 +0200

    Update denon.class.php

--- raspberry/denon.class.php
+++ raspberry/denon.class.php
@@ -5,7 +5,7 @@ class MyDenon
   
   public function getIp()
   {
-    return "192.168.0.120";
+    return "192.168.0.202";
   }
   
   protected function sendCommand($url, $cmd = null, $async = true)
